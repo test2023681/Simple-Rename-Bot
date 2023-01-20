fda1a3954a139071b0e93760e70ea1924a21caa7
test2023681 <117702231+test2023681@users.noreply.github.com>
2023-01-20 23:56:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "22140808"))
 API_HASH = environ.get("API_HASH", "72e39b383c4655d957f67d4ab94cb3aa")
-BOT_TOKEN = environ.get("BOT_TOKEN", "5865869663:AAHSQcV15ugnClWgS_siz8U7RBUWlF53IVk")
+BOT_TOKEN = environ.get("BOT_TOKEN", "5914316069:AAGy-FbwOEQmF1_x7zWz2pYjcVxdrQrWhtM")
 ADMIN = int(environ.get("ADMIN", "5763733318"))          
 CAPTION = environ.get("CAPTION", "join @MoviesNowV2")
 
