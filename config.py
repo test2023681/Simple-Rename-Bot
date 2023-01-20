7fe55005500d96ae92029081149bf42df4558c01
test2023681 <117702231+test2023681@users.noreply.github.com>
2023-01-20 23:54:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "22140808"))
 API_HASH = environ.get("API_HASH", "72e39b383c4655d957f67d4ab94cb3aa")
-BOT_TOKEN = environ.get("BOT_TOKEN", "5922415187:AAHg9Q9HxjR3IhW_suIJcBVUKAgp_drK0qg")
+BOT_TOKEN = environ.get("BOT_TOKEN", "5865869663:AAHSQcV15ugnClWgS_siz8U7RBUWlF53IVk")
 ADMIN = int(environ.get("ADMIN", "5763733318"))          
 CAPTION = environ.get("CAPTION", "join @MoviesNowV2")
 
